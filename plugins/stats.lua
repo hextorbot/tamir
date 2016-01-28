986859109b0425fc090de0fd7515b04eeabfd31e
hextorbot <janjo.urmia@gmail.com>
2016-01-29 01:25:12 +0330

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -141,8 +141,8 @@ return {
     "^[!/]([Ss]tats)$",
     "^[!/]([Ss]tatslist)$",
     "^[!/]([Ss]tats) (group) (%d+)",
-    "^[!/]([Ss]tats) (teleseed)",-- Put everything you like :)
-		"^[!/]([Tt]eleseed)"-- Put everything you like :)
+    "^[!/]([Ss]tats) (teleamir)",-- Put everything you like :)
+		"^[!/]([Tt]eleamir)"-- Put everything you like :)
     }, 
   run = run
 }
