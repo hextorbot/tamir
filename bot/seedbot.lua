ce02f962cb2cdfc19a596a2deb27f986648529ac
hextorbot <janjo.urmia@gmail.com>
2016-01-29 01:29:30 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -221,29 +221,10 @@ function create_config( )
     "all",
     "leave_ban"
     },
-    sudo_users = {119650184(our_id)},--Sudo users
+    sudo_users = {(119650184},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Teleseed v2 - Open Source
-An advance Administration bot based on yagop/telegram-bot 
-
-https://github.com/SEEDTEAM/TeleSeed
-
-Admins
-@iwals [Founder]
-@imandaneshi [Developer]
-@Rondoozle [Developer]
-@seyedan25 [Manager]
-
-Special thanks to
-awkward_potato
-Siyanew
-topkecleon
-Vamptacus
-
-Our channels
-@teleseedch [English]
-@iranseed [persian]
+    about_text = [[
 ]],
     help_text_realm = [[
 Realm Commands:
